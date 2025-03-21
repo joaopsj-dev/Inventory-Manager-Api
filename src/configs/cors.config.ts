13e7c05949ca3f655498bf625f9c499aea9df19b
JsCodeDevlopment <jonatasilva118@gmail.com>
2025-03-20 21:04:00 -0300

    fix: enhance CORS origin handling with normalization and logging

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -1,33 +1,23 @@
-// const whitelistUrls: string[] =
-//   process.env.FRONTEND_URL.split(',').map((url) =>
-//     url.endsWith('/') ? url.slice(0, -1) : url,
-//   ) ?? [];
-
-// export const corsOptions = {
-//   origin: function (
-//     origin: string | undefined,
-//     callback: (error: Error | null, allow?: boolean) => void,
-//   ) {
-//     const normalizedOrigin =
-//       origin && origin.endsWith('/') ? origin.slice(0, -1) : origin;
-
-//     if (whitelistUrls.indexOf(normalizedOrigin) !== -1 || !normalizedOrigin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-// };
-
-const whitelistUrls: string[] = process.env.FRONTEND_URL.split(',') ?? [];
+const whitelistUrls: string[] =
+  process.env.FRONTEND_URL.split(',').map((url) =>
+    url.endsWith('/') ? url.slice(0, -1) : url,
+  ) ?? [];
 
 export const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelistUrls.indexOf(origin) !== -1 || !origin) {
+  origin: function (
+    origin: string | undefined,
+    callback: (error: Error | null, allow?: boolean) => void,
+  ) {
+    console.log('CORS whitelist URLs:', whitelistUrls);
+    console.log('CORS origin:', origin);
+    const normalizedOrigin =
+      origin && origin.endsWith('/') ? origin.slice(0, -1) : origin;
+    console.log('Normalized CORS origin:', normalizedOrigin);
+    if (whitelistUrls.indexOf(normalizedOrigin) !== -1 || !normalizedOrigin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
+  credentials: true,
 };
